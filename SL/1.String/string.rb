32f5e29a2669083ec120bd6e3c46ba95e5132ec8
20WH1A1222 <97152848+20WH1A1222@users.noreply.github.com>
2023-03-09 15:36:08 +0530

    Add files via upload

---
+++ SL/1.String/string.rb
@@ -0,0 +1,7 @@
+def multiple_string(s,n)
+	return s*n
+end
+print multiple_string('a',1),"\n"
+print multiple_string('a',2),"\n"
+print multiple_string('a',3),"\n"
+print multiple_string('a',4),"\n"
